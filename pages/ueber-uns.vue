b9325b7d5c7512d9cbeafc555b677271b53e27f8
Anton <69902586+ant0n7@users.noreply.github.com>
2022-06-19 23:08:18 +0200

    Create description of the team.

--- pages/ueber-uns.vue
+++ pages/ueber-uns.vue
@@ -16,19 +16,44 @@
           <div class="col-md-3"></div>
           <div class="col-md-6 col-12">
             <p>
-              Detken Design ist eine Webdesign Agentur aus Z&uuml;rich. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad iusto quasi itaque culpa illo a perferendis accusantium voluptatibus debitis, molestiae exercitationem fuga atque id, voluptatum repudiandae fugiat officia quo voluptas unde quidem officiis magnam nesciunt eveniet ab! Quasi maiores iure, deserunt error, cum perspiciatis qui sint quod culpa enim deleniti.
+              Detken Design ist eine Webdesign Agentur aus Z&uuml;rich. Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Ad iusto quasi
+              itaque culpa illo a perferendis accusantium voluptatibus debitis,
+              molestiae exercitationem fuga atque id, voluptatum repudiandae
+              fugiat officia quo voluptas unde quidem officiis magnam nesciunt
+              eveniet ab! Quasi maiores iure, deserunt error, cum perspiciatis
+              qui sint quod culpa enim deleniti.
             </p>
           </div>
           <div class="col-md-3"></div>
         </div>
-        <div class="row">
-          <div class="col-md-3"></div>
-          <div class="col-md-6 col-12 team">
-            <h2 class="team-title">Anton Detken</h2>
-            <nuxt-img class="team-img" src="/team/anton_detken_portrait.jpg" />
-            <p class="team-job">Gesch&auml;ftsf&uuml;hrer</p>
+        <div class="team">
+          <div class="row team-title-row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6 col-12">
+              <h2 class="team-title">Anton Detken</h2>
+            </div>
+            <div class="col-md-3"></div>
           </div>
-          <div class="col-md-3"></div>
+          <div class="row team-desc-row">
+            <div class="col-md-3"></div>
+            <div class="col-md-2 col-12">
+              <nuxt-img
+                class="team-img"
+                src="/team/anton_detken_portrait.jpg"
+              />
+              <p class="team-job">Gesch&auml;ftsf&uuml;hrer</p>
+            </div>
+            <div class="col-md-4 col-12">
+              <p class="team-desc">
+                Hero Story. Lorem ipsum dolor sit amet consectetur, adipisicing
+                elit. Nemo velit natus aliquam doloremque? Aperiam, voluptates
+                fugit? Tempora, tempore quam iusto temporibus impedit fugiat.
+                Ullam dolorem tempora deleniti tenetur perspiciatis cupiditate.
+              </p>
+            </div>
+            <div class="col-md-3"></div>
+          </div>
         </div>
       </div>
     </main>
