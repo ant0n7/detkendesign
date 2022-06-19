8ec28bd22091f97bee4b3701c2b9bbb632fe3364
Anton <69902586+ant0n7@users.noreply.github.com>
2022-06-19 13:03:26 +0200

    Create the project page.
    The project page uses a custom component for each reference.

---
+++ components/Reference.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <a
+      :to="projectLink"
+      :title="projectTitle"
+      target="_blank"
+      rel="noopener"
+    >
+      <nuxt-img class="desktop" :src="'/projects/' + projectImage + '-desktop.jpg'" :alt="projectAlt" />
+      <nuxt-img class="mobile" :src="'/projects/' + projectImage + '-mobile.jpg'" :alt="projectAlt" />
+    </a>
+    <h3 class="img-desc">{{ projectTitle }}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    projectTitle: String,
+    projectAlt: String,
+    projectLink: String,
+    projectImage: String
+  }
+}
+</script>
--- pages/projekte.vue
+++ pages/projekte.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
-        <Navbar active="projekte" />
+  <div>
+    <Navbar />
+
+    <div class="row centered-parent">
+      <h1 class="centered">Projekte</h1>
     </div>
+
+    <main>
+      <div class="container gallery project-gallery">
+        <div class="row margin-50">
+          <div class="col-lg-8">
+            <Reference
+              project-title="VESPA Website"
+              project-alt="Screenshot of the current VESPA Website"
+              project-image="vespa"
+              project-link="https://vespa.swiss/"
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <Footer />
+  </div>
 </template>
