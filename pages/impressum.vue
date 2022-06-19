44f2d1f5c396c1250d9344697c00db0db5169372
Anton <69902586+ant0n7@users.noreply.github.com>
2022-06-19 12:08:41 +0200

    Indent lines.
    Parent div was not indented from the template tag.

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,30 +1,30 @@
 <template>
-<div>
+  <div>
     <Navbar />
 
     <main>
-        <div class="container">
-            <div class="row centered-parent">
-                <h1 class="centered">Impressum</h1>
-            </div>
-
-            <div class="row impressum">
-                <div class="col-3"></div>
-                <div class="col-6">
-                    <h2>Gesch&auml;ftsf&uuml;hrer</h2>
-                    <p>Detken Design</p>
-                    <p>Anton Detken</p>
-                    <p>anton@detkendesign.com</p>
-                    <p>Bergheimstrasse 7</p>
-                    <p>8032 Zurich</p>
-                    <p>Switzerland</p>
-                    <p>All rights reserved</p>
-                </div>
-                <div class="col-3"></div>
-            </div>
+      <div class="container">
+        <div class="row centered-parent">
+          <h1 class="centered">Impressum</h1>
         </div>
+
+        <div class="row impressum">
+          <div class="col-3"></div>
+          <div class="col-6">
+            <h2>Gesch&auml;ftsf&uuml;hrer</h2>
+            <p>Detken Design</p>
+            <p>Anton Detken</p>
+            <p>anton@detkendesign.com</p>
+            <p>Bergheimstrasse 7</p>
+            <p>8032 Zurich</p>
+            <p>Switzerland</p>
+            <p>All rights reserved</p>
+          </div>
+          <div class="col-3"></div>
+        </div>
+      </div>
     </main>
 
     <Footer />
-</div>
+  </div>
 </template>
