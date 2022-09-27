099bac593bcd0a131d8dbc95f3419b68c1d0ca9c
Anton <69902586+ant0n7@users.noreply.github.com>
2022-09-27 17:25:00 +0200

    Create Testimonial component.
    Created a new component "Testimonial" for customer testimonials
    and quotes.

---
+++ components/Testimonial.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="quote">
+    <p>
+      <slot />
+    </p>
+    <p>
+      <strong>{{ author }}</strong>
+      <br>
+      {{ authorJob }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.quote {
+  border-left: 5px solid white;
+  padding-left: 1em;
+  margin: 1.5em 10px;
+  font-size: 1.2em;
+  line-height: 1.6;
+  font-style: italic;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    author: String,
+    authorJob: String
+  }
+}
+</script>
