9850067fa003d66600792def2ec53d831790e7a9
Anton Detken <69902586+ant0n7@users.noreply.github.com>
2023-02-07 22:11:19 +0100

    Update Texts

--- pages/index.vue
+++ pages/index.vue
@@ -23,36 +23,47 @@
             anziehen
           </h2>
           <p>
-            Wussten Sie, dass eine Internetseite laut Studien etwa 50
-            Millisekunden hat, um beim Besucher einen guten ersten Eindruck zu
-            hinterlassen? Besonders als Anwaltskanzlei ist es wichtig, diesen
-            kritischen ersten Eindruck zu nutzen. So können sie den Besuchern
-            ihrer Internetseite direkt Vertrauen und Professionalität
-            vermitteln. Profitieren Sie jetzt von unserem Expertenwissen und
-            lassen Sie uns gemeinsam Ihre Vertrauenswürdigkeit, Expertise und
-            Professionalität auf Ihre Online-Präsenz übertragen.
+            Wussten Sie, dass eine Internetseite laut Studien<a
+              class="text-decoration-none"
+              href="https://www.tandfonline.com/doi/abs/10.1080/01449290500330448"
+              target="_blank"
+              ><sup>1</sup></a
+            >
+            etwa 50 Millisekunden hat, um beim Besucher einen guten ersten
+            Eindruck zu hinterlassen? Besonders als Anwaltskanzlei ist es
+            wichtig, diesen kritischen ersten Eindruck zu nutzen. So können Sie
+            den Besuchern Ihrer Internetseite direkt Vertrauenswürdigkeit und
+            Professionalität vermitteln.
           </p>
           <h2 class="h3">Stellen Sie sich bitte einmal folgendes vor…</h2>
           <p>
             Sie sind ein Mandant, der eine Kanzlei sucht, um eine rechtliche
-            Angelegenheit zu regeln. Sie finden durch eine Online-Recherche
-            mehrere Kanzleien in Ihrer Nähe. Dabei fallen ihnen drei dieser
-            Anwaltskanzleien besonders auf. Die erste Internetseite ist sehr
-            veraltet und unprofessionell gestaltet. Bei der zweiten fällt Ihnen
-            eine moderne Gestaltung, aber sie ist sonst nicht sehr intuitiv. Ein
-            professionelles und modernes Design kennzeichnet die dritte
-            Internetseite. Welche der drei würden Sie wählen?
+            Angelegenheit zu klären. Sie finden durch eine Online-Recherche
+            mehrere Kanzleien in Ihrer Nähe. Dabei fallen Ihnen drei
+            Internetseiten dieser Anwaltskanzleien besonders auf. Die erste
+            Internetseite ist sehr veraltet und unprofessionell gestaltet. Bei
+            der zweiten fällt Ihnen eine moderne Gestaltung auf, aber sonst
+            fehlt es ihr an Intuitivität. Ein professionelles und modernes
+            Design kennzeichnet die dritte Internetseite. Welche der drei würden
+            Sie wählen?
           </p>
           <!-- <p>
             Um es in den Worten von Steve Jobs zu sagen: "Es geht nicht nur
             darum, wie es aussieht und sich anfühlt. Design bestimmt wie es
             funktioniert”
           </p> -->
-          <h2 class="h3">Design</h2>
-          <Testimonial author="Steve Jobs" author-job="Gründer, Apple">
-            Es geht nicht nur darum, wie es aussieht und sich anfühlt. Design
-            bestimmt wie es funktioniert
+          <h2 class="h3">Was ist Design?</h2>
+          <Testimonial
+            class="my-3"
+            author="Steve Jobs"
+            author-job="Gründer, Apple"
+          >
+            Design ist nicht nur, wie etwas aussieht und wie es sich anfühlt.
+            Design ist, wie etwas funktioniert.
           </Testimonial>
+          <!-- 
+            Design is a funny word. Some people think design means how it looks. But of course, if you dig deeper, it's really how it works. The design of the Mac wasn't what it looked like, although that was part of it. Primarily, it was how it worked.
+           -->
           <p>
             Professionelles und vertrauenswürdiges Design lassen sich besonders
             darin finden, wie intuitiv die Handhabung in Kombination mit der
@@ -62,14 +73,93 @@
           </p>
 
           <p>
-            Sowohl Mandanten, als auch arbeitssuchende Anwälte können sie
-            deshalb leichter auf Sie aufmerksam machen.
+            Beide Aspekte kombiniert lassen Mandanten und Arbeitnehmer leichter
+            auf Sie aufmerksam werden.
           </p>
 
-          <p>Wir übernehmen diese Aufgabe gerne für Sie.</p>
+          <p>
+            Wir von Detken Design setzen unsere Expertise auf das Gestalten und
+            Erstellen von Internetseiten für Anwaltskanzleien. Leichte
+            Navigierbarkeit und eine psychologisch ansprechende Gestaltung sagen
+            für uns hierbei mehr als 1000 Worte.
+          </p>
           <p>
             Detken Design hat sich auf das Erstellen von Internetseiten von
             Anwaltskanzleien spezialisiert.
+          </p>
+          <!--
+              |-----------------------------------|
+              |                                   |
+              |                                   |
+              |-----------|------------|----------|
+              |           |            |          |
+              |-----------|------------|----------|
+              |           |            |          |
+              |           |            |          |
+              |-----------|------------|----------|
+              |                                   |
+              |-----------------------------------|
+              Following table should have the same format as above -->
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="row" colspan="3">
+                  <h3 class="text-center">
+                    Unsere Designsprache steigert Ihren Erfolg
+                  </h3>
+                  <p class="opacity-50 text-center h4">
+                    Einige Vorteile unserer Arbeit für Sie sind ...
+                  </p>
+                </th>
+              </tr>
+              <tr>
+                <th scope="row"><h4>Ihre Zeit</h4></th>
+                <th scope="row"><h4>&Auml;ssere Wahrnehmung</h4></th>
+                <th scope="row"><h4>Vorsprung</h4></th>
+              </tr>
+              <tr>
+                <td>
+                  <p>
+                    Wir erstellen Ihre Internetseite so, dass Sie sich auf Ihr
+                    Kerngeschäft konzentrieren können.
+                  </p>
+                </td>
+                <td>
+                  <p>
+                    Mit einer überzeugenden Website stärken Sie das Image Ihrer
+                    Kanzlei.
+                  </p>
+                  <p>
+                    Potenzielle Mandanten und Arbeitnehmer nehmen Sie so in
+                    einem noch besseren Licht wahr.
+                  </p>
+                </td>
+                <td>
+                  <p>Erhöhen Sie Ihre Verfügbarkeit ohne mehr Anwesenheit.</p>
+                  <p>
+                    Gewinnen Sie Kunden und Arbeitnehmer durch Ihre
+                    leistungsstarke Webseite rund um die Uhr.
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td scope="col" colspan="3">
+                  <p>
+                    Kombiniert führen diese und weitere Vorteile zu einem
+                    nachhaltig größeren Erfolg Ihrer Kanzlei!
+                  </p>
+                </td>
+              </tr>
+            </thead>
+          </table>
+
+          <p>Profitieren Sie jetzt von unserer Erfahrung.</p>
+          <p>
+            Lassen Sie uns gemeinsam durch einen positiven und bleibenden
+            Eindruck den Erfolg Ihrer Kanzlei sichern und erhöhen.
+            <a href="https://calendly.com/detkendesign/15min" target="_blank"
+              >Unverbindliches Erstgespräch</a
+            >
           </p>
         </TextWrapper>
       </div>
@@ -80,6 +170,19 @@
 <style scoped>
 #welcome-text {
   text-align: center;
+}
+
+table p,
+table h3,
+table h4,
+table h5 {
+  text-align: center;
+  margin-bottom: 0;
+}
+
+td,
+th {
+  border: 1px solid #dee2e67e;
 }
 </style>
 
