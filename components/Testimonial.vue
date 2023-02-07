4f24b74c0e42f7925fec7b3ff6bf446021645585
Anton Detken <69902586+ant0n7@users.noreply.github.com>
2023-02-07 22:43:38 +0100

    Optimize left border of Testimonial.

--- components/Testimonial.vue
+++ components/Testimonial.vue
@@ -5,7 +5,7 @@
     </p>
     <p>
       <strong>{{ author }}</strong>
-      <br>
+      <br />
       {{ authorJob }}
     </p>
   </div>
@@ -13,7 +13,7 @@
 
 <style scoped>
 .quote {
-  border-left: 5px solid white;
+  border-left: 1px solid rgba(222, 226, 230, 0.49412);
   padding-left: 1em;
   margin: 1.5em 10px;
   font-size: 1.2em;
@@ -26,7 +26,7 @@
 export default {
   props: {
     author: String,
-    authorJob: String
-  }
+    authorJob: String,
+  },
 }
 </script>
